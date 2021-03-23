35319b7b6cd4bcf957b7fbf336bdada7a14005b0
Markus Mulkahainen <markus.mulkahainen@hotmail.com>
2021-03-23 18:53:36 +0200

    fix bug with mdn_url

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 var open = require('open');
 var req = require('superagent');
 var arg = require('minimist')(process.argv.slice(2), { boolean: ['o', 'open'] });
-var url = 'https://developer.mozilla.org/en-US/search.json';
+var baseURL = 'https://developer.mozilla.org';
 var o = arg.o || arg.open || (arg._[0] == 'open' ? arg._.shift() : false);
 
-var request = url +
+var request = baseURL + '/en-US/search.json' +
     '?q=' + encodeURIComponent(arg._.join(' ')) +
     '&page=' + encodeURIComponent(arg.p || arg.page || 1) +
     '&per_page=' + encodeURIComponent(arg.l || arg.limit || o ? 1 : 5);
@@ -15,9 +15,9 @@ if (arg.t || arg.topic) request += '&topic=' + encodeURIComponent(arg.t || arg.t
 
 req.get(request).end(function(err, res){
     if (!err && res.body.documents) {
-        if (o) open(res.body.documents[0].url);
+        if (o) open(baseURL + res.body.documents[0].mdn_url);
         else console.log('\n' + res.body.documents.map(function(doc){
-            return doc.title + '\n' + doc.url;
+            return doc.title + '\n' + baseURL + doc.mdn_url;
         }).join('\n\n') + '\n');
     }
 });
